ef599d21624770ddeac1390816a74a74e9ecbfa3
Ryan Shofner <ryan@letsgolearn.com>
2016-05-22 23:40:40 -0700

    add fromUser test

--- tests/PrivateChannelTest.php
+++ tests/PrivateChannelTest.php
@@ -16,6 +16,7 @@ class PrivateChannelTest extends TestCase
 
     protected $laravel;
     protected $sentinel;
+    protected $internal;
 
 
     public function tearDown()
@@ -33,6 +34,7 @@ class PrivateChannelTest extends TestCase
         parent::setUp();
         $this->laravel  = new PrivateChannel(new BaseAuth(new LaravelAuth()));
         $this->sentinel = new PrivateChannel(new Sentinel(new SentinelAuth()));
+        $this->internal = PrivateChannel::forUser(33);
     }
 
 
@@ -45,6 +47,17 @@ class PrivateChannelTest extends TestCase
         $pusher->shouldReceive('socket_auth')->withArgs([$this->laravel->getChannel(), 123])->andReturn(true);
         $this->laravel->setPusher($pusher);
         $this->assertTrue($this->laravel->auth('private-notifications-123', 123));
+    }
+
+    /**
+     * @test
+     */
+    public function it_authenticates_internal_user_sockets()
+    {
+        $pusher = m::mock('Pusher');
+        $pusher->shouldReceive('socket_auth')->withArgs([$this->internal->getChannel(), 33])->andReturn(true);
+        $this->internal->setPusher($pusher);
+        $this->assertTrue($this->internal->auth('private-notifications-33', 33));
     }
 
 
